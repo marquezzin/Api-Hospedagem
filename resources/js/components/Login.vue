ad57a86f05f39dc0a4833eb986d51a026fe86791
Gabriel Marques da Rocha <gabriel.rocha@tridia.com.br>
2025-01-07 15:52:29 -0300

    :sparkles: feat: Instalação do Vue.js na aplicacação e começo da implementação do componente de Login.

---
+++ resources/js/components/Login.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Login do Vue</div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+     @vite(['resources/js/app.js']) {{-- botar o css se for necessario --}}
+</head>
+<body class="antialiased">
+    <div id="app"> {{-- Elemento com o id "app" --}}
+        <example-component></example-component>
+    </div>
+</body>
+</html>
