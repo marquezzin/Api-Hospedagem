3961c9368d7e5642fff295b4300d9a21ce5fa186
Gabriel Marques da Rocha <gabriel.rocha@tridia.com.br>
2025-02-03 13:36:48 -0300

    :ok_hand: style: Update README and change project title.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Hosting System</title>
 
      @vite(['resources/js/app.js']) {{-- botar o css se for necessario --}}
 <style>
